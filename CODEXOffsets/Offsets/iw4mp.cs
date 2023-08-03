98e91ff5dbd6d60200430e3efd6266f2adb09afa
PierreDuv <frozenphenix78@gmail.com>
2023-08-03 19:21:09 +0200

    feat : update offset

--- CODEXOffsets/Offsets/iw4mp.cs
+++ CODEXOffsets/Offsets/iw4mp.cs
@@ -7,7 +7,7 @@ namespace CODEXOffsets.Offsets
 {
     class iw4mp : IOffsets
     {
-        public int cbuf_addtext { get { return 0x563D10; } }
+        public int cbuf_addtext { get { return 0x563BE0; } }
         public int nop_address { get { return 0x0; } }
     }
 }
